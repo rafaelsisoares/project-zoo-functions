0f17c4fa672b507506d124a2007a2e103e6ecc97
Rafael Soares <r.soares2015@outlook.com>
2022-07-29 15:11:51 -0300

    Fixed lint issues

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,16 +1,16 @@
-const handlerElephants = require("../src/handlerElephants");
+const handlerElephants = require('../src/handlerElephants');
 
-describe("Testes da função HandlerElephants", () => {
-  it("Testa se a função retorna undefined caso não receba parâmetros", () => {
+describe('Testes da função HandlerElephants', () => {
+  it('Testa se a função retorna undefined caso não receba parâmetros', () => {
     expect(handlerElephants()).toBe(undefined);
   });
-  it("Testa se a função retorna uma frase caso o parâmetro passado não seja uma string", () => {
+  it('Testa se a função retorna uma frase caso o parâmetro passado não seja uma string', () => {
     expect(handlerElephants(0)).toBe(
-      "Parâmetro inválido, é necessário uma string"
+      'Parâmetro inválido, é necessário uma string',
     );
   });
-  it("Testa se ao passar o parâmetro count, a função retorna a quantidade de elefantes que vivem no zoo", () => {
-    expect(handlerElephants("count")).toBe(4);
+  it('Testa se ao passar o parâmetro count, a função retorna a quantidade de elefantes que vivem no zoo', () => {
+    expect(handlerElephants('count')).toBe(4);
   });
   it('Testa se ao passar o parâmetro names, a função retorna um array com todos os nomes dos elefantes', () => {
     expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
