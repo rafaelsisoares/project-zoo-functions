1fc126b1a40aa0d322ba4c6199302623aa67b5d2
Rafael Soares <r.soares2015@outlook.com>
2022-07-28 21:25:52 -0300

    Quinto requisito resolvido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
-}
+  if (animal === undefined) {
+    const allSpecies = {};
+    data.species.reduce((actual, next) => {
+      allSpecies[actual.name] = actual.residents.length;
+      allSpecies[next.name] = next.residents.length;
+      return next;
+    });
+    return allSpecies;
+  }
 
+  const { specie, sex } = animal;
+  if (sex === undefined) {
+    return data.species.find((obj) => obj.name === specie).residents.length;
+  }
+  return data.species.find((obj) => obj.name === specie).residents.filter((desc) =>
+    desc.sex === sex).length;
+}
 module.exports = countAnimals;
