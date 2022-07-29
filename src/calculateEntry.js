10a9055f047f1567a60abb694ce3d3d2ddc95af3
Rafael Soares <r.soares2015@outlook.com>
2022-07-29 18:24:10 -0300

    Oitavo requisito resolvido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.filter((visitor) => visitor.age < 18).length,
+    adult: entrants.filter((visitor) => visitor.age >= 18 && visitor.age < 50).length,
+    senior: entrants.filter((visitor) => visitor.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+
+  const { child, adult, senior } = countEntrants(entrants);
+  return (child * data.prices.child) + (adult * data.prices.adult) + (senior * data.prices.senior);
 }
+
+console.log(calculateEntry({}));
 
 module.exports = { calculateEntry, countEntrants };
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,64 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
+
+const animals = data.species.map((animal) => animal.name);
+
+const isMonday = {
+  Monday: {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  },
+};
+
+const weekDays = [
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const fullSchedule = () => {
+  const scheduleComplete = {};
+  Object.assign(scheduleComplete, isMonday);
+  weekDays.forEach((day) => {
+    scheduleComplete[day] = {
+      officeHour: `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`,
+      exhibition: data.species
+        .filter((animal) => animal.availability.includes(day))
+        .map((animal) => animal.name),
+    },
+  });
+  return scheduleComplete;
+};
+
+const weekDaySchedule = (day) => {
+  if (day === 'Monday') {
+    return isMonday;
+  }
+  if (weekDays.includes(day)) {
+    return {
+      [day]: {
+        officeHour: `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`,
+        exhibition: data.species
+          .filter((animal) => animal.availability.includes(day))
+          .map((animal) => animal.name),
+      },
+    };
+  }
+  return fullSchedule();
+};
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (animals.includes(scheduleTarget)) {
+    const animalTarget = data.species.find(
+      (animal) => animal.name === scheduleTarget
+    );
+    return animalTarget.availability;
+  }
+  return weekDaySchedule(scheduleTarget);
 }
+
+console.log(getSchedule("Monday"));
 
 module.exports = getSchedule;
