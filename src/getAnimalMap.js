f2ea3248bbcb33c15d65cd4c20dac9fda7e4e903
Rafael Soares <r.soares2015@outlook.com>
2022-08-01 21:43:45 -0300

    Primeiras partes do decimo quarto requisito feitas

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined || options.includeNames === undefined) {
+    return {
+      NE: data.species.filter((animal) => animal.location === 'NE').map((animal) => animal.name),
+      NW: data.species.filter((animal) => animal.location === 'NW').map((animal) => animal.name),
+      SE: data.species.filter((animal) => animal.location === 'SE').map((animal) => animal.name),
+      SW: data.species.filter((animal) => animal.location === 'SW').map((animal) => animal.name),
+    };
+  }
 }
 
 module.exports = getAnimalMap;
